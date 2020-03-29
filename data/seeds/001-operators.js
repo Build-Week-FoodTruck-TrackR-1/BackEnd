51f75415adfabcb2c9ace33630594e5e5d8e3792
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2020-03-29 04:39:31 -0300

    added 3 new operators

--- data/seeds/001-operators.js
+++ data/seeds/001-operators.js
@@ -8,7 +8,10 @@ exports.seed = function(knex) {
       return knex('operators').insert([
         {username: 'juanz', email: 'juan.zamora@gmail.com', password: bcrypt.hashSync('zamora', 8)},
         {username: 'gyang', email: 'gregory.zang@gmail.com', password: bcrypt.hashSync('yanggang', 8)},
-        {username: 'noelg', email: 'noel.gallo@gmail.com', password: bcrypt.hashSync('gallo', 8)}
+        {username: 'noelg', email: 'noel.gallo@gmail.com', password: bcrypt.hashSync('gallo', 8)},
+        {username: 'aladinm', email: 'aladin.moktar@gmail.com', password: bcrypt.hashSync('moktar', 8) },
+        {username: 'ananyac', email: 'ananya.chopra@gmail.com', password: bcrypt.hashSync('chopra', 8)},
+        {username: 'bharatz', email: 'bharat.zafar@gmail.com', password: bcrypt.hashSync('zafar', 8)}
       ]);
     });
 };

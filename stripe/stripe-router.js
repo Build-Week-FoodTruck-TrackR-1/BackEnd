78f9cd151e06ba44e2373b72fd96afb584a4c815
Jevon Cochran <jevon.cochran@gmail.com>
2020-05-14 03:17:57 -0300

    added name column to diners table, stripe integration

---
+++ stripe/stripe-router.js
@@ -0,0 +1,23 @@
+// endpoints for: /api/stripe
+
+const express = require('express');
+const router = express.Router();
+
+const env = require("dotenv").config({ path: "./.env" });
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+router.post('/create-customer', (req, res) => {
+    let customer = req.body;
+
+    stripe.customers.create(customer, function(err, customer) {
+        if (err) {
+            console.log(err);
+        } else if (customer) {
+            console.log(`success: ${customer}`);
+        } else {
+            console.log('Something went wrong');
+        }
+    })
+})
+
+module.exports = router;

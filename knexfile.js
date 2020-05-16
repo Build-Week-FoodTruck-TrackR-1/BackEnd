c0ba83baca6e48e920bbf4f315a5191006f227ca
Jevon Cochran <jevon.cochran@gmail.com>
2020-05-16 09:35:10 -0300

    add PG database URL

--- knexfile.js
+++ knexfile.js
@@ -65,5 +65,17 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     }
-  }
+  },
+  
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  } 
 };

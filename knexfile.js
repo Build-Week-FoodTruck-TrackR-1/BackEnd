e83939b20390d59c5ad0eb42c74823a5c6a8ee50
Jevon Cochran <jevon.cochran@gmail.com>
2020-05-16 11:08:56 -0300

    deleted node modules

--- knexfile.js
+++ knexfile.js
@@ -66,16 +66,4 @@ module.exports = {
       directory: './data/seeds'
     }
   },
-  
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    }
-  } 
 };
